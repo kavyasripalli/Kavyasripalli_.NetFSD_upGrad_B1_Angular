5cf3a4a653e4911906675146dbbf19f2f9d48d6b
Kavya <113791019+kavyasripalli@users.noreply.github.com>
2026-04-01 01:19:30 +0530

    Commit

---
+++ ASP.NetCore_Assignments/MVC_Assignment_3/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Assignment_3.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
