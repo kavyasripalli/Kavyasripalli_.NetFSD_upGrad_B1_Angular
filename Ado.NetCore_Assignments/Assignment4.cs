05ebc723350b5b839c090c3fb1733aebee854603
KAVYA SRI <113791019+kavyasripalli@users.noreply.github.com>
2026-04-08 02:35:34 +0530

    Update Assignment4.cs

--- Ado.NetCore_Assignments/Assignment4.cs
+++ Ado.NetCore_Assignments/Assignment4.cs
@@ -6,7 +6,7 @@ namespace Ado.Net_Assignments
 {
     internal class Assignment4
     {
-        string connectionString = "Data Source=LAPTOP-RRH9QFR3\\SQLEXPRESS01;Initial Catalog=StudentDB;Integrated Security=True;Trust Server Certificate=True";
+        string connectionString = "Data Source=DESKTOP-DA9RDQ5\\SQLEXPRESS;Initial Catalog=StudentDB;Integrated Security=True;Trust Server Certificate=True";
 
         public void CreateOrder()
         {
